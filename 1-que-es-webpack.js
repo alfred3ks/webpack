1286dad302eda288594d380d40d820f8ab8725df
Alfredo Sánchez <alfred3ks@gmail.com>
2022-08-06 10:20:47 +0200

    Que es webpack.

---
+++ 1-que-es-webpack.js
@@ -0,0 +1,9 @@
+/*
+
+Es una herramienta que nos ayuda a preparar nuestro codigo para enviarlo a produccion, mas al detalle, webpack es un module bundle.
+
+Nacio en el 2012, a dia de hoy es usado por muchas empresas como su core.
+
+A dia de hoy es una herramienta muy importante a dia de hoy sobre todo en el frontend.
+
+*/
