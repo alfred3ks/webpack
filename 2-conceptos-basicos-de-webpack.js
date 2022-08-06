a8fea8542d6b709cd108dbbf49173f6f8d4bd408
Alfredo Sánchez <alfred3ks@gmail.com>
2022-08-06 10:28:46 +0200

    Conceptos básicos.

---
+++ 2-conceptos-basicos-de-webpack.js
@@ -0,0 +1,11 @@
+/*
+
+Veamos las principales particularidades que nos va a dar webpack para poder trabajar con el:
+
+- Definimos a webpack como un paquete de modulos estaticos para aplicaciones de JS modernas.
+- Webpack construye un grafico de dependencias que mapea cada modulo para convertirlo en uno o mas modulos segun sea el caso.
+
+-Tenemos algunas particularidades que tenemos que tene ren cuenta:
+Desde webpack 4 ya no dependemos de tener un archivo de configuracion, sino que podemos trabajar con un standar que se crea. Pero para esto debemos que tener un punto de entrada dentro de una carpeta con un archivo que se suele llamar index.js. Tambien debemos trabajar con un punto de salida que es donde se produciran los archivos de salida.
+
+*/
