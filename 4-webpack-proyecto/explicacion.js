66075b98a35d07479b06532ba8be1d98660d5707
Alfredo Sánchez <alfred3ks@gmail.com>
2022-08-06 11:30:43 +0200

    Creacion del proyecto y instalacion de webpack.

---
+++ 4-webpack-proyecto/explicacion.js
@@ -0,0 +1,19 @@
+/*
+
+Vamos a ver un proyecto para ver todas las particularidades de webpack.
+Haremos un pequeño porfolio de demo.
+Arrancaremos con un proyecto ya creado llamado js-portfolio, se puede descargar desde el repo:
+
+https://github.com/gndx/js-portfolio
+
+Este es un proyecto construido en html css y js, nada de framework ni librerias, no esta contruido con webpack. Lo vamos a preparar con webpack.
+
+Lo primero que haremos es instalar webpack en este proyecto.
+npm install webpack webpack-cli -D
+
+Ejecutamos luego la compilacion en desarrollo:
+npx webpack --mode production
+
+Vemos como nos genera la carpeta dist.
+
+*/
