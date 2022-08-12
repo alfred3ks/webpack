2eafaf23b4c72bf1ddfa8f03cabf91061e7d09f5
Alfredo Sánchez <alfred3ks@gmail.com>
2022-08-12 17:43:35 +0200

    Fin...

---
+++ 26-optimizacion-webpack-para-react/explicacion.js
@@ -0,0 +1,12 @@
+/*
+
+Vamos a añadir unas ultimas configuraciones para optimizar nuestro proyecto:
+
+npm install css-minimizer-webpack-plugin terser-webpack-plugin clean-webpack-plugin -D
+
+Vamos a crearnos un archivo de webpack para modo produccion junto con los script en el package.json.
+
+Fin...
+
+
+*/
